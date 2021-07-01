62c0d0475e62a8704eae7711ccb7b7d954c7a696
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-07-01 23:50:52 +0600

    pythonping.py add

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kaj%c#(f4sr(0e(2qpurnhj@7$c^+drwn9i4psv$$4-5)&h_(9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
